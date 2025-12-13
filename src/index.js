306db4f36083ca71b049b0307cb2cf726dc42d7f
TinyDotPink <jurgeit1978@gmail.com>
2025-12-13 18:20:39 +0100

    Java Script Good!

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 function displayPoem(response) {
+  console.log(response.data.answer);
   let poemBox = document.querySelector("#poem");
   new Typewriter(poemBox, {
     strings: response.data.answer,
@@ -9,13 +10,13 @@ function displayPoem(response) {
 function generatePoem(event) {
   event.preventDefault();
 
-  let instructionInput = document.querySelector("#userTopic");
-  console.log(instructionInput);
+  let userInput = document.querySelector(".instructions");
+  console.log(userInput.value);
 
   let apiKey = "5fobe02b0dff93d79ecc47a523d0f3t0";
-  let prompt = `Generate a German poem about ${instructionInput}`;
+  let prompt = `Generate a German poem about ${userInput.value}`;
   let context =
-    "You are an romantic poem expert who loves to write short poems";
+    "You are a romantic Poem expert and love to write short poems. You mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   console.log("Generating Poem...");
