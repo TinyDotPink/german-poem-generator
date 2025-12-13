4237ecffb9809760c319e78411248d78c1a84e16
TinyDotPink <jurgeit1978@gmail.com>
2025-12-13 16:30:15 +0100

    API call - not working

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
-function generatePoem(event) {
-  event.preventDefault();
+function displayPoem(response) {
   let poemBox = document.querySelector("#poem");
   new Typewriter(poemBox, {
-    strings: "Alles geben die Götter, die unendlichen",
+    strings: response.data.answer,
     autoStart: true,
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let instructionInput = document.querySelector("#userTopic");
+  console.log(instructionInput);
+
+  let apiKey = "5fobe02b0dff93d79ecc47a523d0f3t0";
+  let prompt = `Generate a German poem about ${instructionInput}`;
+  let context =
+    "You are an romantic poem expert who loves to write short poems";
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("Generating Poem...");
+  console.log(`Prompt: ${prompt}`);
+  console.log(`Context: ${context}`);
+
+  axios.get(apiURL).then(displayPoem);
 }
 let poemForm = document.querySelector("#poem-generator-form");
 console.log(poemForm);
